ec23b2ac81cd49ea86116af54343a80e7f1eb7f0
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-11-02 22:15:04 +0200

    added FAQ

---
+++ app.js
@@ -0,0 +1,14 @@
+// ? Task:
+
+
+// Solution 1
+
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
+
+// ! Explanation: 
